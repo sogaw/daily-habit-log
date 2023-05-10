c378096b0477441715cf1bf88de1961ac1f8d84a
sogaw <natto.and.eggs+0@gmail.com>
2023-05-10 14:18:35 +0900

    fix

--- backend/script/src/seed.ts
+++ backend/script/src/seed.ts
@@ -1,13 +1,14 @@
+import { auth } from "script/setup";
+
+import { User } from "@/datasource";
 import { datasourceContext } from "@/resolver";
 
 const datasource = datasourceContext();
 
 (async () => {
-  const users = await datasource.users.findManyByQuery((ref) => ref);
-  for (const user of users) {
-    const habits = await user.habits.findManyByQuery((ref) => ref);
-    const sprints = await user.sprints.findManyByQuery((ref) => ref);
-    await Promise.all(habits.map((habit) => habit.recursiveDestroy()));
-    await Promise.all(sprints.map((sprint) => sprint.recursiveDestroy()));
-  }
+  const userOne = await auth.getUserByEmail("user-1@example.com").catch(() => undefined);
+  if (userOne) return;
+
+  const { uid } = await auth.createUser({ email: "user-1@example.com", password: "Password00", emailVerified: true });
+  await User.create(datasource.users, { id: uid, name: "UserOne", iconPath: "" }).save();
 })();
