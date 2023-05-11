749f2b65c33fec0abe19fab9267361813c10aaa7
sogaw <natto.and.eggs+0@gmail.com>
2023-05-11 11:45:56 +0900

    fix

--- frontend/src/hooks/use-try-fn.ts
+++ frontend/src/hooks/use-try-fn.ts
@@ -1,25 +1,33 @@
 import { useState } from "react";
 
-export const useTryFn = <T = void>(
-  fn: (input: T) => void,
-  options?: { onCompleted?: () => void; onError?: (e?: unknown) => void }
+export const useTryFn = <T, U = void>(
+  fn: (input: U) => Promise<T>,
+  options?: { onCompleted?: (res: T) => void; onError?: (e?: unknown) => void }
 ) => {
+  const [response, setResponse] = useState<T>();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<unknown>();
 
-  const tryFn = async (input: T) => {
+  const tryFn = async (input: U) => {
+    let res;
     try {
       setLoading(true);
-      await fn(input);
+
+      res = await fn(input);
+      setResponse(res);
       setError(undefined);
-      if (options?.onCompleted) await options.onCompleted();
+
+      if (options?.onCompleted) await options.onCompleted(res);
     } catch (e) {
+      setResponse(undefined);
       setError(e);
+
       if (options?.onError) await options.onError(e);
     } finally {
       setLoading(false);
     }
+    return res;
   };
 
-  return [tryFn, { loading, error }] as const;
+  return [tryFn, { response, loading, error }] as const;
 };
