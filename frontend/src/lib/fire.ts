2e13ad135ad092eabd39d6bdfb29cabd24250387
sogaw <natto.and.eggs+0@gmail.com>
2023-05-20 11:49:03 +0900

    fix

--- frontend/src/lib/fire.ts
+++ frontend/src/lib/fire.ts
@@ -1,3 +1,4 @@
+import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 import { connectAuthEmulator, getAuth } from "firebase/auth";
 import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
@@ -19,4 +20,8 @@ if (!import.meta.env.PROD) {
   });
   connectFirestoreEmulator(getFirestore(), "127.0.0.1", 8080);
   connectStorageEmulator(getStorage(), "127.0.0.1", 9199);
+}
+
+if (import.meta.env.PROD) {
+  getAnalytics();
 }
