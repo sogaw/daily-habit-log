97e792915360af208dd7227521c095fa78475ad7
sogaw <natto.and.eggs+0@gmail.com>
2023-06-04 14:50:49 +0900

    remove sprints view from frontend

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,37 +1,22 @@
-import { Box, Button, Flex, Select, Stack, Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
+import { Box, Button, Stack, Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import { useLocalStorage } from "react-use";
 
 import { Fallback } from "@/components/Fallback";
 import { HabitsList } from "@/components/habit/HabitsList";
 import { Layout } from "@/components/Layout";
-import { SprintsList } from "@/components/sprint/SprintsList";
 import { TweetCreateForm } from "@/components/tweet/TweetCreateForm";
 import { TweetsList } from "@/components/tweet/TweetsList";
-import { SprintsFilter } from "@/generated/gql/graphql";
 import { Guard } from "@/hocs/guard";
 import { useHabits } from "@/hooks/habit/use-habits";
-import { useSprints } from "@/hooks/sprint/use-sprints";
 import { useTweets } from "@/hooks/tweet/use-tweets";
 
 const Home = Guard("AfterOnboard", () => {
   const navigate = useNavigate();
 
   const [tabIndex, setTabIndex] = useLocalStorage("daily-habit-log.home.tab", 0);
-  const [sprintsFilter, setSprintsFilter] = useLocalStorage<SprintsFilter>(
-    "daily-habit-log.home.sprints-filter",
-    "TODAY"
-  );
 
   const { habits, loading: habitsLoading, error: habitsError } = useHabits({ skip: tabIndex != 0 });
-
-  const {
-    sprints,
-    pageInfo: sprintsPageInfo,
-    loading: sprintsLoading,
-    error: sprintsError,
-    fetchMore: sprintsFetchMore,
-  } = useSprints({ skip: tabIndex != 1, filter: sprintsFilter as SprintsFilter });
 
   const {
     tweets,
@@ -39,14 +24,13 @@ const Home = Guard("AfterOnboard", () => {
     loading: tweetsLoading,
     error: tweetsError,
     fetchMore: tweetsFetchMore,
-  } = useTweets({ skip: tabIndex != 2 });
+  } = useTweets({ skip: tabIndex != 1 });
 
   return (
     <Layout pt="0">
       <Tabs position="relative" defaultIndex={tabIndex} onChange={setTabIndex}>
         <TabList position="sticky" top="14" zIndex="1" bg="white">
           <Tab fontWeight="semibold">Habits</Tab>
-          <Tab fontWeight="semibold">Sprints</Tab>
           <Tab fontWeight="semibold">Tweets</Tab>
         </TabList>
 
@@ -65,50 +49,6 @@ const Home = Guard("AfterOnboard", () => {
                 )}
                 {habits && <HabitsList habits={habits} mode="edit" />}
               </Fallback>
-            </Stack>
-          </TabPanel>
-
-          <TabPanel px="2">
-            <Stack spacing="4">
-              <Flex justify="space-between" align="center">
-                <Box w="32">
-                  <Select
-                    size="sm"
-                    rounded="md"
-                    value={sprintsFilter}
-                    onChange={(e) => setSprintsFilter(e.target.value as SprintsFilter)}
-                  >
-                    <option value="TODAY">Today</option>
-                    <option value="ALL">All</option>
-                  </Select>
-                </Box>
-
-                <Button alignSelf="end" size="sm" colorScheme="green" onClick={() => navigate("/sprints/new")}>
-                  New sprint
-                </Button>
-              </Flex>
-
-              <Stack>
-                <Fallback loading={sprintsLoading} error={sprintsError}>
-                  {sprints?.length == 0 && (
-                    <Box alignSelf="center" fontWeight="semibold" fontSize="sm" color="gray.500">
-                      No Sprints
-                    </Box>
-                  )}
-                  {sprints && <SprintsList sprints={sprints} mode="edit" />}
-                  {sprintsPageInfo?.hasNextPage && (
-                    <Button
-                      alignSelf="center"
-                      variant="ghost"
-                      size="sm"
-                      onClick={() => sprintsFetchMore()}
-                      isDisabled={sprintsLoading}
-                    >
-                      More
-                    </Button>
-                  )}
-                </Fallback>
-              </Stack>
             </Stack>
           </TabPanel>
 
